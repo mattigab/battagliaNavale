e79c803dffab7924eebb6994f3dd441d41544a49
Mattia Gabbrielli <mattia.gabbrielli@gmail.com>
2015-11-17 16:18:41 +0100

    SI CAZZO

--- web/js/data.js
+++ web/js/data.js
@@ -3,12 +3,15 @@
  */
 
 var type = ["nave","acqua","natante"];
-var map = [];
+var N = 15;
+var map =new Array(N);
 
-for (var ii=0; ii<15; ii++)
-{
-    for (var jj=0; jj<15; jj++)
-    {
+for(var i=0; i<N; i++){
+    map[i]=new Array(N);
+}
+
+for(var ii=0; ii<N; ii++){
+    for (var jj=0;jj<N; jj++){
         map[ii][jj] = type[Math.floor(Math.random()*3)];
     }
 }
