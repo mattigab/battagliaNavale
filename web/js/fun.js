2f4aa456f485a1409fb8b95b7067cdd6ae27336e
Mattia Gabbrielli <mattia.gabbrielli@gmail.com>
2015-11-17 12:20:45 +0100

    TAMBERL!!!

--- web/js/fun.js
+++ web/js/fun.js
@@ -1,15 +1,16 @@
 function load()
 {
-	var cells = ["","",""];
-	var myTab = document.getEelementById("myTab");
+	//var cells = ["","",""];
+	var ids= ["1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"];
+	var myTabDiv = document.getElementById("myTab");
 	var tabHTML = "<table>";
 	for (var r = 0; r<15;r++)
 	{
 		tabHTML=tabHTML+"<tr>";
 		for (var c = 0; c<15; c++)
-		{	
-			tabHTML=tabHTML+"<td>";
-			tabHTML=tabHTML+"AA"
+		{
+			tabHTML=tabHTML+"<td id="+ids[r] + ids[c]+">";
+			tabHTML=tabHTML+ "(" +ids[r]+","+ids[c] + ") ";
 			tabHTML=tabHTML+"</td>";
 
 		}
@@ -17,5 +18,5 @@ function load()
 	}
 
 	tabHTML=tabHTML+"</table>";
-
+	myTabDiv.innerHTML = tabHTML;
 }
