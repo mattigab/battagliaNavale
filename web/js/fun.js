6a20d5a1064453a7dcea4ac47a71479d0a04b78c
Mattia Gabbrielli <mattia.gabbrielli@gmail.com>
2015-11-17 18:00:58 +0100

    risolto problema: chi cazzo ha deciso di mettere le lettere?!?!? MANNAJA

--- web/js/fun.js
+++ web/js/fun.js
@@ -1,7 +1,6 @@
 function load()
 {
-	//var cells = ["","",""];
-	var ids= ["1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"];
+	//var ids= ["1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"];
 	var myTabDiv = document.getElementById("myTab");
 	var tabHTML = "<table class='myTab'>";
 	for (var r = 0; r<15;r++)
@@ -9,10 +8,8 @@ function load()
 		tabHTML=tabHTML+"<tr>";
 		for (var c = 0; c<15; c++)
 		{
-			tabHTML=tabHTML+"<td id="+ids[r] + ids[c]+" onclick='shoot("+"'"+ids[r]+"',"+"'"+ids[c].toString()+"')';>";
+			tabHTML=tabHTML+"<td id="+r+c+" onclick='shoot("+r+","+c+")'>";
 			tabHTML=tabHTML+ "<img src=./img/fog.png class='img-responsive'>";
-
-			//"(" +ids[r]+","+ids[c] + ") ";
 			tabHTML=tabHTML+"</td>";
 
 		}
@@ -25,8 +22,23 @@ function load()
 
 function shoot(r,c)
 {
-	var ids= ["1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"];
+	//var ids= ["1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"];
 	var myCell = document.getElementById(r.toString()+c.toString());
+
+	//alert(r.toString()+c.toString())
+	if(map[r][c] == "nave")
+	{
+
+		myCell.innerHTML = "<img src=./img/nave.png class='img-responsive'>";
+	}
+	else if(map[r][c] == "natante")
+	{
+		myCell.innerHTML = "<img src=./img/natante.png class='img-responsive'>";
+	}
+	else
+	{
+		myCell.innerHTML = "<img src=./img/water.png class='img-responsive'>";
+	}
 	/*if(map[ids.indexOf(r.toString())][ids.indexOf(c.toString())] == "nave")
 	{
 		myCell.innerHTML = "<img src=./img/nave.png class='img-responsive'>";
@@ -39,8 +51,5 @@ function shoot(r,c)
 	{
 		myCell.innerHTML = "<img src=./img/water.png class='img-responsive'>";
 	}*/
-
-
-	alert(ids.indexOf(c.toString()));
 
 }
