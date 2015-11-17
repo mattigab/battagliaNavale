6e7acb9035bf3deb85c1dc875ff20982070b6e51
Fabrizio <brizio.dane@live.it>
2015-11-17 16:54:56 +0100

    bella

--- web/js/fun.js
+++ web/js/fun.js
@@ -3,14 +3,14 @@ function load()
 	//var cells = ["","",""];
 	var ids= ["1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"];
 	var myTabDiv = document.getElementById("myTab");
-	var tabHTML = "<table>";
+	var tabHTML = "<table class='myTab'>";
 	for (var r = 0; r<15;r++)
 	{
 		tabHTML=tabHTML+"<tr>";
 		for (var c = 0; c<15; c++)
 		{
-			tabHTML=tabHTML+"<td id="+ids[r] + ids[c]+" onclick='shoot("+ids[r]+","+ids[c]+")';>";
-			tabHTML=tabHTML+ "<img src=./img/fog.png>";
+			tabHTML=tabHTML+"<td id="+ids[r] + ids[c]+" onclick='shoot("+"'"+ids[r]+"',"+"'"+ids[c].toString()+"')';>";
+			tabHTML=tabHTML+ "<img src=./img/fog.png class='img-responsive'>";
 
 			//"(" +ids[r]+","+ids[c] + ") ";
 			tabHTML=tabHTML+"</td>";
@@ -27,17 +27,20 @@ function shoot(r,c)
 {
 	var ids= ["1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"];
 	var myCell = document.getElementById(r.toString()+c.toString());
-	if(map[ids.indexOf(r.toString())][ids.indexOf(c.toString())] == "nave")
+	/*if(map[ids.indexOf(r.toString())][ids.indexOf(c.toString())] == "nave")
 	{
-		myCell.innerHTML = "<img src=./img/nave.png>";
+		myCell.innerHTML = "<img src=./img/nave.png class='img-responsive'>";
 	}
 	else if(map[ids.indexOf(r.toString())][ids.indexOf(c.toString())] == "natante")
 	{
-		myCell.innerHTML = "<img src=./img/natante.png>";
+		myCell.innerHTML = "<img src=./img/natante.png class='img-responsive'>";
 	}
 	else
 	{
-		myCell.innerHTML = "<img src=./img/water.png>";
-	}
+		myCell.innerHTML = "<img src=./img/water.png class='img-responsive'>";
+	}*/
+
+
+	alert(ids.indexOf(c.toString()));
 
 }
